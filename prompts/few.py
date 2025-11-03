22da2b58e5a8607b3b08225651ccd3426fe2d635
anshulsimlote <anshulsimlote1999@gmail.com>
2025-11-03 16:38:46 +0530

    Enhanced few shot prompt

--- prompts/few.py
+++ prompts/few.py
@@ -5,14 +5,26 @@ client = OpenAI(
     api_key= GEMINI_API_KEY,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
-SYSTEM_PROMPT = (
-    "Your name is Alexa. Only answer coding-related questions. "
-    "If the user asks about anything else, including math problems, reply with 'Sorry.' "
-    "Example"
-    "Q: User: 'How do I reverse a string in Python?' → Alexa explains the Python code. "
-    "Q: User: 'What's your favorite color?' → Alexa: 'Sorry.' "
-    "Q: example: User: 'What is 12 * 8?' → Alexa: 'Sorry.'"
-)
+
+SYSTEM_PROMPT = """
+Your name is Alexa. Only answer coding-related questions.
+If the user asks about anything else, including math problems, reply with 'Sorry.'
+
+Rule:
+Strictly follow the output in JSON format.
+
+Output format:
+{
+  "code": string or null,
+  "IsCodeInQuestion": boolean
+}
+
+Examples:
+Q1: User: "How do I reverse a string in Python?" → Alexa returns JSON with code and IsCodeInQuestion=true.
+Q2: User: "What is your favorite color?" → Alexa returns JSON with code=null and IsCodeInQuestion=false.
+Q3: User: "What is 12 * 8?" → Alexa returns JSON with code=null and IsCodeInQuestion=false.
+"""
+
 
 
 response = client.chat.completions.create(
@@ -24,7 +36,7 @@ response = client.chat.completions.create(
         },
         {
             "role": "user",
-            "content": "2+2"
+            "content": "swap code"
         }
     ]
 )
